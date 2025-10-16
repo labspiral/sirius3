2084fc26ceb641caf70e220548b17b13fa684eae
hcchoi <hcchoi@spirallab.co.kr>
2025-10-16 15:09:37 +0900

    added) editor_mof_xy demo

---
+++ demos/editor_mof_xy/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Demos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnCreateEntities = new System.Windows.Forms.Button();
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.btnStartEncoderSimulation = new System.Windows.Forms.Button();
+            this.btnStopEncoderSimulation = new System.Windows.Forms.Button();
+            this.siriusEditorControl1 = new SpiralLab.Sirius3.UI.WinForms.SiriusEditorControl();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btnCreateEntities);
+            this.flowLayoutPanel1.Controls.Add(this.btnStartEncoderSimulation);
+            this.flowLayoutPanel1.Controls.Add(this.btnStopEncoderSimulation);
+            this.flowLayoutPanel1.Controls.Add(this.btnStartStop);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(2254, 48);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // btnCreateEntities
+            // 
+            this.btnCreateEntities.Location = new System.Drawing.Point(3, 3);
+            this.btnCreateEntities.Name = "btnCreateEntities";
+            this.btnCreateEntities.Size = new System.Drawing.Size(155, 42);
+            this.btnCreateEntities.TabIndex = 0;
+            this.btnCreateEntities.Text = "Create Entities";
+            this.btnCreateEntities.UseVisualStyleBackColor = true;
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Location = new System.Drawing.Point(618, 3);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(221, 42);
+            this.btnStartStop.TabIndex = 3;
+            this.btnStartStop.Text = "Start/Stop";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            // 
+            // btnStartEncoderSimulation
+            // 
+            this.btnStartEncoderSimulation.Location = new System.Drawing.Point(164, 3);
+            this.btnStartEncoderSimulation.Name = "btnStartEncoderSimulation";
+            this.btnStartEncoderSimulation.Size = new System.Drawing.Size(221, 42);
+            this.btnStartEncoderSimulation.TabIndex = 1;
+            this.btnStartEncoderSimulation.Text = "Start Encoder (Simulation)";
+            this.btnStartEncoderSimulation.UseVisualStyleBackColor = true;
+            // 
+            // btnStopEncoderSimulation
+            // 
+            this.btnStopEncoderSimulation.Location = new System.Drawing.Point(391, 3);
+            this.btnStopEncoderSimulation.Name = "btnStopEncoderSimulation";
+            this.btnStopEncoderSimulation.Size = new System.Drawing.Size(221, 42);
+            this.btnStopEncoderSimulation.TabIndex = 2;
+            this.btnStopEncoderSimulation.Text = "Stop Encoder (Simulation)";
+            this.btnStopEncoderSimulation.UseVisualStyleBackColor = true;
+            // 
+            // siriusEditorControl1
+            // 
+            this.siriusEditorControl1.BackColor = System.Drawing.SystemColors.Control;
+            this.siriusEditorControl1.DIExt1 = null;
+            this.siriusEditorControl1.DILaserPort = null;
+            this.siriusEditorControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.siriusEditorControl1.DOExt1 = null;
+            this.siriusEditorControl1.DOExt2 = null;
+            this.siriusEditorControl1.DOLaserPort = null;
+            this.siriusEditorControl1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siriusEditorControl1.Laser = null;
+            this.siriusEditorControl1.Location = new System.Drawing.Point(0, 48);
+            this.siriusEditorControl1.Margin = new System.Windows.Forms.Padding(0);
+            this.siriusEditorControl1.Marker = null;
+            this.siriusEditorControl1.Name = "siriusEditorControl1";
+            this.siriusEditorControl1.PowerMeter = null;
+            this.siriusEditorControl1.Scanner = null;
+            this.siriusEditorControl1.Size = new System.Drawing.Size(2254, 1590);
+            this.siriusEditorControl1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2254, 1638);
+            this.Controls.Add(this.siriusEditorControl1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Sirius3 Demo - (c)SpiralLAB";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button btnCreateEntities;
+        private SpiralLab.Sirius3.UI.WinForms.SiriusEditorControl siriusEditorControl1;
+        private Button btnStartEncoderSimulation;
+        private Button btnStopEncoderSimulation;
+        private Button btnStartStop;
+    }
+}
---
+++ demos/editor_mof_xy/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+
+using SpiralLab.Sirius3.Document;
+using SpiralLab.Sirius3.Scanner;
+using SpiralLab.Sirius3.IO;
+using SpiralLab.Sirius3.Scanner.Rtc;
+using SpiralLab.Sirius3.PowerMeter;
+using SpiralLab.Sirius3.Laser;
+using SpiralLab.Sirius3.Marker;
+using SpiralLab.Sirius3.Entity;
+using System.Text;
+using SpiralLab.Sirius3.Entity.Hatch;
+using System.Diagnostics;
+
+#if OPENTK3
+using OpenTK;
+using DVec2 = OpenTK.Vector2d;
+using DVec3 = OpenTK.Vector3d;
+using DVec4 = OpenTK.Vector4d;
+using DMat3 = OpenTK.Matrix3d;
+using DMat4 = OpenTK.Matrix4d;
+#elif OPENTK4
+using OpenTK.Mathematics;
+using DVec2 = OpenTK.Mathematics.Vector2d;
+using DVec3 = OpenTK.Mathematics.Vector3d;
+using DVec4 = OpenTK.Mathematics.Vector4d;
+using DMat3 = OpenTK.Mathematics.Matrix3d;
+using DMat4 = OpenTK.Mathematics.Matrix4d;
+#endif
+
+namespace Demos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+
+            this.btnCreateEntities.Click += BtnCreateEntities_Click;
+            this.btnStartStop.Click += BtnStartStop_Click;
+            this.btnStartEncoderSimulation.Click += BtnStartEncoderSimulation_Click;
+            this.btnStopEncoderSimulation.Click += BtnStopEncoderSimulation_Click;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            EditorHelper.CreateDevices(out IRtc rtc, out ILaser laser, out IDInput dInExt1, out IDInput dInLaserPort, out IDOutput dOutExt1, out IDOutput dOutExt2, out IDOutput dOutLaserPort, out IPowerMeter powerMeter, out IMarker marker);
+
+            siriusEditorControl1.Scanner = rtc;
+
+            siriusEditorControl1.Laser = laser;
+
+            siriusEditorControl1.DIExt1 = dInExt1;
+            siriusEditorControl1.DOExt1 = dOutExt1;
+            siriusEditorControl1.DOExt2 = dOutExt2;
+            siriusEditorControl1.DILaserPort = dInLaserPort;
+            siriusEditorControl1.DOLaserPort = dOutLaserPort;
+
+            siriusEditorControl1.PowerMeter = powerMeter;
+
+            siriusEditorControl1.Marker = marker;
+
+            marker.Ready(siriusEditorControl1.Document, siriusEditorControl1.View, rtc, laser, powerMeter);
+        }
+
+        private void BtnCreateEntities_Click(object sender, EventArgs e)
+        {
+            var document = siriusEditorControl1.Document;
+            document.ActNew();
+
+            CreateEntities();
+        }
+
+        void CreateEntities()
+        {
+            var document = siriusEditorControl1.Document;
+            var marker = siriusEditorControl1.Marker;
+            var rtc = siriusEditorControl1.Scanner as IRtc;
+
+            // Create mof begin 
+            // with encoder reset
+            var mofBegin = EntityFactory.CreateMoFBegin(RtcMoFModes.XY, true);
+            document.ActivePage?.ActiveLayer?.AddChild(mofBegin);
+
+            /*      
+                    *                     |
+                    *                     |
+                    *                     |
+                    *                     |
+                    *     . .             |
+                    *      .        |     |
+                    *       .       | |   |
+                    *     .         | | | |
+                    *  ----.--▯--@--|-|-|-+-------------------    => ENC +
+                    *       .       | | | |                       => MOVING DIRECTION 
+                    *     .         | |   |
+                    *      .        |     |
+                    *        .            |
+                    *      .              |
+                    *                     |
+                    *                     |
+                    *                     |
+                    */
+            // adjust RtcEncoderWaitConditions contition to marks at scanner center area
+
+            double x1 = -1;
+            var mofWait1 = EntityFactory.CreateMoFWait(RtcEncoders.EncX, RtcEncoderWaitConditions.Over, -x1);
+            document.ActivePage?.ActiveLayer?.AddChild(mofWait1);
+            var line1 = EntityFactory.CreateLine(x1, 10, x1, -10);
+            document.ActivePage?.ActiveLayer?.AddChild(line1);
+
+            double x2 = -5;
+            var mofWait2 = EntityFactory.CreateMoFWait(RtcEncoders.EncX, RtcEncoderWaitConditions.Over, -x2);
+            document.ActivePage?.ActiveLayer?.AddChild(mofWait2);
+            var line2 = EntityFactory.CreateLine(x2, 15, x2, -15);
+            document.ActivePage?.ActiveLayer?.AddChild(line2);
+
+            double x3 = -10;
+            var mofWait3 = EntityFactory.CreateMoFWait(RtcEncoders.EncX, RtcEncoderWaitConditions.Over, -x3);
+            document.ActivePage?.ActiveLayer?.AddChild(mofWait3);
+            var line3 = EntityFactory.CreateLine(x3, 20, x3, -20);
+            document.ActivePage?.ActiveLayer?.AddChild(line3);
+
+            double x4 = -15;
+            var mofWait4 = EntityFactory.CreateMoFWait(RtcEncoders.EncX, RtcEncoderWaitConditions.Over, -x4);
+            document.ActivePage?.ActiveLayer?.AddChild(mofWait4);
+            var spiral = EntityFactory.CreateSpiral(x4, 0, 2, 4, 0, 5, true);
+            document.ActivePage?.ActiveLayer?.AddChild(spiral);
+
+            double x5 = -20;
+            var mofWait5 = EntityFactory.CreateMoFWait(RtcEncoders.EncX, RtcEncoderWaitConditions.Over, -x5);
+            document.ActivePage?.ActiveLayer?.AddChild(mofWait5);
+            var dataMatrix = EntityFactory.CreateDataMatrix("SIRIUS2", EntityBarcode2DBase.Barcode2DCells.Outline, 3, 4, 4);
+            dataMatrix.RotateZ(90);
+            dataMatrix.Name = "MyBarcode1";
+            dataMatrix.Translate(x5, -10);
+            document.ActivePage?.ActiveLayer?.AddChild(dataMatrix);
+
+            var text = EntityFactory.CreateText("Arial", FontStyle.Bold, "SIRIUS2", 4);
+            text.RotateZ(90);
+            text.Translate(x5, 10);
+            document.ActivePage?.ActiveLayer?.AddChild(text);
+
+            double x6 = -40;
+            var mofWait6 = EntityFactory.CreateMoFWait(RtcEncoders.EncX, RtcEncoderWaitConditions.Over, -x6);
+            document.ActivePage?.ActiveLayer?.AddChild(mofWait6);
+            double xRange = 2;
+            double yRange = 30;
+            var rnd = new Random();
+            var pts = new List<DVec2>(20);
+            for (int i = 0; i < 20; i++)
+            {
+                double x = x6 + rnd.NextDouble() * (xRange + xRange) - xRange;
+                double y = rnd.NextDouble() * (yRange + yRange) - yRange;
+                pts.Add(new DVec2(x, y));
+            }
+            var points = EntityFactory.CreatePoints(pts);
+            document.ActivePage?.ActiveLayer?.AddChild(points);
+
+            var mofEnd = EntityFactory.CreateMoFEnd(DVec2.Zero);
+            document.ActivePage?.ActiveLayer?.AddChild(mofEnd);
+
+            Debug.Assert(rtc.IsMoF);
+            var rtcMoF = rtc as IRtcMoF;
+            Debug.Assert(rtcMoF != null);
+            Debug.Assert(rtcMoF.EncXCountsPerMm != 0);
+            //Debug.Assert(rtcMoF.EncYCountsPerMm != 0);
+        }
+
+        private void BtnStartStop_Click(object sender, EventArgs e)
+        {
+            var marker = siriusEditorControl1.Marker;
+
+            if (marker.IsBusy)
+            {
+                marker.Stop();
+                marker.Reset();
+            }
+            else
+            {
+                marker.Reset();
+                marker.Ready(siriusEditorControl1.Document);
+                marker.Start();
+            }
+        }
+
+        private void BtnStartEncoderSimulation_Click(object sender, EventArgs e)
+        {
+            var rtc = siriusEditorControl1.Scanner as IRtc;
+            if (rtc.IsBusy)
+                return;
+
+            Debug.Assert(rtc.IsMoF);
+            var rtcMoF = rtc as IRtcMoF;
+
+            // Start simulated encoders as x= 1, y=0 mm/s by rtcMoF.CtlMofEncoderSpeed(1, 0);
+            rtcMoF.CtlMofEncoderSpeed(1, 0);
+            // or
+            // Edit 'Simulated x speed at MoF = 1' at propertygrid of scanner(RTC) page
+            // and
+            // Marker.Start
+        }
+
+        private void BtnStopEncoderSimulation_Click(object sender, EventArgs e)
+        {
+            var rtc = siriusEditorControl1.Scanner as IRtc;
+
+            Debug.Assert(rtc.IsMoF);
+            var rtcMoF = rtc as IRtcMoF;
+
+            // Deactivated simulated encoders 
+            rtcMoF.CtlMofEncoderSpeed(0, 0);
+
+        }
+
+    }
+}
