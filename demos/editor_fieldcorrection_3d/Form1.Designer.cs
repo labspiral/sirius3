81247b34afbfd9435fc52225ba452b8b7ece2590
hcchoi <hcchoi@spirallab.co.kr>
2025-10-17 13:01:08 +0900

    added) editor_fieldcorrection_3d demo

---
+++ demos/editor_fieldcorrection_3d/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Demos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnCreateGrids0 = new System.Windows.Forms.Button();
+            this.btnCorrection3D = new System.Windows.Forms.Button();
+            this.btnSelectTable = new System.Windows.Forms.Button();
+            this.siriusEditorControl1 = new SpiralLab.Sirius3.UI.WinForms.SiriusEditorControl();
+            this.btnCreateGrids5 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btnCreateGrids0);
+            this.flowLayoutPanel1.Controls.Add(this.btnCreateGrids5);
+            this.flowLayoutPanel1.Controls.Add(this.btnCorrection3D);
+            this.flowLayoutPanel1.Controls.Add(this.btnSelectTable);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(2254, 48);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // btnCreateGrids0
+            // 
+            this.btnCreateGrids0.Location = new System.Drawing.Point(3, 3);
+            this.btnCreateGrids0.Name = "btnCreateGrids0";
+            this.btnCreateGrids0.Size = new System.Drawing.Size(174, 42);
+            this.btnCreateGrids0.TabIndex = 0;
+            this.btnCreateGrids0.Text = "Create Grids (Z=0)";
+            this.btnCreateGrids0.UseVisualStyleBackColor = true;
+            // 
+            // btnCorrection3D
+            // 
+            this.btnCorrection3D.Location = new System.Drawing.Point(363, 3);
+            this.btnCorrection3D.Name = "btnCorrection3D";
+            this.btnCorrection3D.Size = new System.Drawing.Size(145, 42);
+            this.btnCorrection3D.TabIndex = 1;
+            this.btnCorrection3D.Text = "Correction 3D";
+            this.btnCorrection3D.UseVisualStyleBackColor = true;
+            // 
+            // btnSelectTable
+            // 
+            this.btnSelectTable.Location = new System.Drawing.Point(514, 3);
+            this.btnSelectTable.Name = "btnSelectTable";
+            this.btnSelectTable.Size = new System.Drawing.Size(145, 42);
+            this.btnSelectTable.TabIndex = 2;
+            this.btnSelectTable.Text = "Select Table";
+            this.btnSelectTable.UseVisualStyleBackColor = true;
+            // 
+            // siriusEditorControl1
+            // 
+            this.siriusEditorControl1.BackColor = System.Drawing.SystemColors.Control;
+            this.siriusEditorControl1.DIExt1 = null;
+            this.siriusEditorControl1.DILaserPort = null;
+            this.siriusEditorControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.siriusEditorControl1.DOExt1 = null;
+            this.siriusEditorControl1.DOExt2 = null;
+            this.siriusEditorControl1.DOLaserPort = null;
+            this.siriusEditorControl1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siriusEditorControl1.Laser = null;
+            this.siriusEditorControl1.Location = new System.Drawing.Point(0, 48);
+            this.siriusEditorControl1.Margin = new System.Windows.Forms.Padding(0);
+            this.siriusEditorControl1.Marker = null;
+            this.siriusEditorControl1.Name = "siriusEditorControl1";
+            this.siriusEditorControl1.PowerMeter = null;
+            this.siriusEditorControl1.Scanner = null;
+            this.siriusEditorControl1.Size = new System.Drawing.Size(2254, 1590);
+            this.siriusEditorControl1.TabIndex = 2;
+            // 
+            // btnCreateGrids5
+            // 
+            this.btnCreateGrids5.Location = new System.Drawing.Point(183, 3);
+            this.btnCreateGrids5.Name = "btnCreateGrids5";
+            this.btnCreateGrids5.Size = new System.Drawing.Size(174, 42);
+            this.btnCreateGrids5.TabIndex = 3;
+            this.btnCreateGrids5.Text = "Create Grids (Z=5)";
+            this.btnCreateGrids5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2254, 1638);
+            this.Controls.Add(this.siriusEditorControl1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Sirius3 Demo - (c)SpiralLAB";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button btnCreateGrids0;
+        private SpiralLab.Sirius3.UI.WinForms.SiriusEditorControl siriusEditorControl1;
+        private Button btnCorrection3D;
+        private Button btnSelectTable;
+        private Button btnCreateGrids5;
+    }
+}
---
+++ demos/editor_fieldcorrection_3d/Form1.cs
@@ -0,0 +1,241 @@
+using System;
+
+using SpiralLab.Sirius3.Document;
+using SpiralLab.Sirius3.Scanner;
+using SpiralLab.Sirius3.IO;
+using SpiralLab.Sirius3.Scanner.Rtc;
+using SpiralLab.Sirius3.PowerMeter;
+using SpiralLab.Sirius3.Laser;
+using SpiralLab.Sirius3.Marker;
+using SpiralLab.Sirius3.Entity;
+using System.Text;
+using SpiralLab.Sirius3.Entity.Hatch;
+using SpiralLab.Sirius3.UI.WinForms;
+using System.Diagnostics;
+
+#if OPENTK3
+using OpenTK;
+using DVec2 = OpenTK.Vector2d;
+using DVec3 = OpenTK.Vector3d;
+using DVec4 = OpenTK.Vector4d;
+using DMat3 = OpenTK.Matrix3d;
+using DMat4 = OpenTK.Matrix4d;
+#elif OPENTK4
+using OpenTK.Mathematics;
+using DVec2 = OpenTK.Mathematics.Vector2d;
+using DVec3 = OpenTK.Mathematics.Vector3d;
+using DVec4 = OpenTK.Mathematics.Vector4d;
+using DMat3 = OpenTK.Mathematics.Matrix3d;
+using DMat4 = OpenTK.Mathematics.Matrix4d;
+#endif
+
+namespace Demos
+{
+    public partial class Form1 : Form
+    {
+        int rows = 5;
+        int cols = 5;
+        double fieldSize;
+        double rowInterval;
+        double colInterval;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+            this.btnCreateGrids0.Click += BtnCreateGrids0_Click;
+            this.btnCreateGrids5.Click += BtnCreateGrids5_Click;
+            this.btnCorrection3D.Click += BtnCorrection3D_Click;
+            this.btnSelectTable.Click += BtnSelectTable_Click;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            EditorHelper.CreateDevices(out IRtc rtc, out ILaser laser, out IDInput dInExt1, out IDInput dInLaserPort, out IDOutput dOutExt1, out IDOutput dOutExt2, out IDOutput dOutLaserPort, out IPowerMeter powerMeter, out IMarker marker);
+
+
+            siriusEditorControl1.Scanner = rtc;
+
+            siriusEditorControl1.Laser = laser;
+
+            siriusEditorControl1.DIExt1 = dInExt1;
+            siriusEditorControl1.DOExt1 = dOutExt1;
+            siriusEditorControl1.DOExt2 = dOutExt2;
+            siriusEditorControl1.DILaserPort = dInLaserPort;
+            siriusEditorControl1.DOLaserPort = dOutLaserPort;
+
+            siriusEditorControl1.PowerMeter = powerMeter;
+
+            siriusEditorControl1.Marker = marker;
+
+            marker.Ready(siriusEditorControl1.Document, siriusEditorControl1.View, rtc, laser, powerMeter);
+        }
+
+        private void BtnCreateGrids0_Click(object sender, EventArgs e)
+        {
+            var document = siriusEditorControl1.Document;
+            document.ActNew();
+
+            // approx. field area 
+            int index = 0;
+            var fov = NativeMethods.ReadIni<double>(EditorHelper.ConfigFileName, $"RTC{index}", "FOV", 100.0);
+            fieldSize = fov * 0.9; //reduce for effective field only
+
+            siriusEditorControl1.EditorCtrl.View.FovSize = new DVec3(fov, fov, 0);
+            siriusEditorControl1.EditorCtrl.View.FovCenter = new DVec3(0, 0, 0);
+
+            //create grids
+            rows = 3;
+            cols = 3;
+            rowInterval = (double)Math.Floor(fieldSize / (rows - 1) * 1000.0f) / 1000.0f;
+            colInterval = (double)Math.Floor(fieldSize / (cols - 1) * 1000.0f) / 1000.0f;
+
+            var left = -colInterval * (int)(cols / 2);
+            var right = colInterval * (int)(cols / 2);
+            var bottom = -rowInterval * (int)(rows / 2);
+            var top = rowInterval * (int)(rows / 2);
+
+            var entities = new List<IEntity>((int)(rows * cols));
+
+            for (int row = 0; row < rows; row++)
+            {
+                var start = new DVec2(left, (bottom + rowInterval * row));
+                var end = new DVec2(right, (bottom + rowInterval * row));
+                var line = EntityFactory.CreateLine(start, end);
+                entities.Add(line);
+            }
+            for (int col = 0; col < cols; col++)
+            {
+                var start = new DVec2((left + colInterval * col), bottom);
+                var end = new DVec2((left + colInterval * col), top);
+                var line = EntityFactory.CreateLine(start, end);
+                entities.Add(line);
+            }
+
+            var group = EntityFactory.CreateGroup($"{rows}x{cols} {rowInterval}x{colInterval}mm", entities);
+            //double z = 0;
+            //group.Translate(0, 0, z);
+            document?.ActivePage?.ActiveLayer?.AddChild(group);
+        }
+        private void BtnCreateGrids5_Click(object sender, EventArgs e)
+        {
+            var document = siriusEditorControl1.Document;
+            document.ActNew();
+
+            // approx. field area 
+            int index = 0;
+            var fov = NativeMethods.ReadIni<double>(EditorHelper.ConfigFileName, $"RTC{index}", "FOV", 100.0);
+            fieldSize = fov * 0.9; //reduce for effective field only
+
+            siriusEditorControl1.EditorCtrl.View.FovSize = new DVec3(fov, fov, 5);
+            siriusEditorControl1.EditorCtrl.View.FovCenter = new DVec3(0, 0, 2.5);
+
+
+            //create grids
+            rows = 3;
+            cols = 3;
+            rowInterval = (double)Math.Floor(fieldSize / (rows - 1) * 1000.0f) / 1000.0f;
+            colInterval = (double)Math.Floor(fieldSize / (cols - 1) * 1000.0f) / 1000.0f;
+
+            var left = -colInterval * (int)(cols / 2);
+            var right = colInterval * (int)(cols / 2);
+            var bottom = -rowInterval * (int)(rows / 2);
+            var top = rowInterval * (int)(rows / 2);
+
+            var entities = new List<IEntity>((int)(rows * cols));
+
+            for (int row = 0; row < rows; row++)
+            {
+                var start = new DVec2(left, (bottom + rowInterval * row));
+                var end = new DVec2(right, (bottom + rowInterval * row));
+                var line = EntityFactory.CreateLine(start, end);
+                entities.Add(line);
+            }
+            for (int col = 0; col < cols; col++)
+            {
+                var start = new DVec2((left + colInterval * col), bottom);
+                var end = new DVec2((left + colInterval * col), top);
+                var line = EntityFactory.CreateLine(start, end);
+                entities.Add(line);
+            }
+
+            var group = EntityFactory.CreateGroup($"{rows}x{cols} {rowInterval}x{colInterval}mm", entities);
+            double z = 5;
+            group.Translate(0, 0, z);
+            document?.ActivePage?.ActiveLayer?.AddChild(group);
+        }
+
+        private void BtnCorrection3D_Click(object sender, EventArgs e)
+        {
+            Debug.Assert(rowInterval == colInterval);
+
+            var rtc = siriusEditorControl1.Scanner as IRtc;
+            float lower = 0;
+            float upper = 5;
+
+            var rtcCorrection3D = new RtcCorrection3D(rtc.KFactor, rows, cols, rowInterval, upper, lower, rtc.CorrectionFiles[(int)rtc.PrimaryHeadTable].FileName, string.Empty);
+            double left = -colInterval * (double)(int)(cols / 2);
+            double top = rowInterval * (double)(int)(rows / 2);
+            
+            //lower
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    // input dx, dy position error
+                    rtcCorrection3D.AddRelative(
+                        row, col,
+                        new DVec3(left + col * colInterval, top - row * rowInterval, upper),
+                        DVec3.Zero
+                        );
+                }
+            }
+
+            //upper
+            var rand = new Random();
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    // input dx, dy position error
+                    rtcCorrection3D.AddRelative(
+                        row, col,
+                        new DVec3(left + col * colInterval, top - row * rowInterval, upper),
+                        new DVec3(
+                            rand.Next(20) / 1000.0 - 0.01,
+                            rand.Next(20) / 1000.0 - 0.01,
+                            0)
+                        );
+                }
+            }
+
+            var form = new RtcCorrection3DForm(rtc, rtcCorrection3D);
+            form.ShowDialog();
+        }
+
+        private void BtnSelectTable_Click(object sender, EventArgs e)
+        {
+            var rtc = siriusEditorControl1.Scanner as IRtc;
+
+            var dlg = new OpenFileDialog();
+            dlg.Title = "Open Correction File";
+            dlg.InitialDirectory = SpiralLab.Sirius3.Config.CorrectionPath;
+            dlg.Filter = "ct5 correction file (*.ct5)|*.ct5|ctb correction file (*.ctb)|*.ctb|All Files (*.*)|*.*";
+            dlg.DefaultExt = "ct5";
+
+            DialogResult result = dlg.ShowDialog();
+            if (result != DialogResult.OK)
+                return;
+
+            bool success = true;
+            var currentTable = rtc.PrimaryHeadTable;
+            rtc.CtlLoadCorrectionFile(currentTable, dlg.FileName);
+            rtc.CtlSelectCorrection(currentTable);
+            if (success)
+                System.Windows.Forms.MessageBox.Show($"Target correction file is load/selected at Table1");
+            else
+                System.Windows.Forms.MessageBox.Show($"Fail to load/select correction file");
+        }
+
+    }
+}
