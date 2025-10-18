6496022d7510fac1c438adb8b8f9a046815a3c29
labspiral <labspiral@gmail.com>
2025-10-18 17:28:32 +0900

    added) editor_alc demo

---
+++ demos/editor_automatic_laser_control/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Demos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnSetVelocity = new System.Windows.Forms.Button();
+            this.siriusEditorControl1 = new SpiralLab.Sirius3.UI.WinForms.SiriusEditorControl();
+            this.btnActualVelocity = new System.Windows.Forms.Button();
+            this.btnPositionDependent = new System.Windows.Forms.Button();
+            this.btnSpotDistance = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btnSetVelocity);
+            this.flowLayoutPanel1.Controls.Add(this.btnActualVelocity);
+            this.flowLayoutPanel1.Controls.Add(this.btnPositionDependent);
+            this.flowLayoutPanel1.Controls.Add(this.btnSpotDistance);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(2254, 55);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // btnSetVelocity
+            // 
+            this.btnSetVelocity.Location = new System.Drawing.Point(3, 3);
+            this.btnSetVelocity.Name = "btnSetVelocity";
+            this.btnSetVelocity.Size = new System.Drawing.Size(118, 49);
+            this.btnSetVelocity.TabIndex = 0;
+            this.btnSetVelocity.Text = "Set velocity";
+            this.btnSetVelocity.UseVisualStyleBackColor = true;
+            // 
+            // siriusEditorControl1
+            // 
+            this.siriusEditorControl1.BackColor = System.Drawing.SystemColors.Control;
+            this.siriusEditorControl1.DIExt1 = null;
+            this.siriusEditorControl1.DILaserPort = null;
+            this.siriusEditorControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.siriusEditorControl1.DOExt1 = null;
+            this.siriusEditorControl1.DOExt2 = null;
+            this.siriusEditorControl1.DOLaserPort = null;
+            this.siriusEditorControl1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siriusEditorControl1.Laser = null;
+            this.siriusEditorControl1.Location = new System.Drawing.Point(0, 55);
+            this.siriusEditorControl1.Margin = new System.Windows.Forms.Padding(0);
+            this.siriusEditorControl1.Marker = null;
+            this.siriusEditorControl1.Name = "siriusEditorControl1";
+            this.siriusEditorControl1.PowerMeter = null;
+            this.siriusEditorControl1.Scanner = null;
+            this.siriusEditorControl1.Size = new System.Drawing.Size(2254, 1583);
+            this.siriusEditorControl1.TabIndex = 2;
+            // 
+            // btnActualVelocity
+            // 
+            this.btnActualVelocity.Location = new System.Drawing.Point(127, 3);
+            this.btnActualVelocity.Name = "btnActualVelocity";
+            this.btnActualVelocity.Size = new System.Drawing.Size(118, 49);
+            this.btnActualVelocity.TabIndex = 1;
+            this.btnActualVelocity.Text = "Actual velocity";
+            this.btnActualVelocity.UseVisualStyleBackColor = true;
+            // 
+            // btnPositionDependent
+            // 
+            this.btnPositionDependent.Location = new System.Drawing.Point(251, 3);
+            this.btnPositionDependent.Name = "btnPositionDependent";
+            this.btnPositionDependent.Size = new System.Drawing.Size(185, 49);
+            this.btnPositionDependent.TabIndex = 2;
+            this.btnPositionDependent.Text = "Position dependent";
+            this.btnPositionDependent.UseVisualStyleBackColor = true;
+            // 
+            // btnSpotDistance
+            // 
+            this.btnSpotDistance.Location = new System.Drawing.Point(442, 3);
+            this.btnSpotDistance.Name = "btnSpotDistance";
+            this.btnSpotDistance.Size = new System.Drawing.Size(132, 49);
+            this.btnSpotDistance.TabIndex = 3;
+            this.btnSpotDistance.Text = "Spot distance";
+            this.btnSpotDistance.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2254, 1638);
+            this.Controls.Add(this.siriusEditorControl1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Sirius3 Demo - (c)SpiralLAB";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private SpiralLab.Sirius3.UI.WinForms.SiriusEditorControl siriusEditorControl1;
+        private Button btnSetVelocity;
+        private Button btnActualVelocity;
+        private Button btnPositionDependent;
+        private Button btnSpotDistance;
+    }
+}
---
+++ demos/editor_automatic_laser_control/Form1.cs
@@ -0,0 +1,251 @@
+using System;
+
+using SpiralLab.Sirius3.Document;
+using SpiralLab.Sirius3.Scanner;
+using SpiralLab.Sirius3.IO;
+using SpiralLab.Sirius3.Scanner.Rtc;
+using SpiralLab.Sirius3.PowerMeter;
+using SpiralLab.Sirius3.Laser;
+using SpiralLab.Sirius3.Marker;
+using SpiralLab.Sirius3.Entity;
+using System.Text;
+using SpiralLab.Sirius3.Entity.Hatch;
+using Microsoft.Extensions.Logging;
+using SpiralLab.Sirius3;
+using SpiralLab.Sirius3.Mathematics;
+using SpiralLab.Sirius3.Scanner.Rtc.SyncAxis;
+using System.Diagnostics;
+
+#if OPENTK3
+using OpenTK;
+using DVec2 = OpenTK.Vector2d;
+using DVec3 = OpenTK.Vector3d;
+using DVec4 = OpenTK.Vector4d;
+using DMat3 = OpenTK.Matrix3d;
+using DMat4 = OpenTK.Matrix4d;
+#elif OPENTK4
+using OpenTK.Mathematics;
+using DVec2 = OpenTK.Mathematics.Vector2d;
+using DVec3 = OpenTK.Mathematics.Vector3d;
+using DVec4 = OpenTK.Mathematics.Vector4d;
+using DMat3 = OpenTK.Mathematics.Matrix3d;
+using DMat4 = OpenTK.Mathematics.Matrix4d;
+#endif
+
+namespace Demos
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+
+            btnSetVelocity.Click += BtnSetVelocity_Click;
+            btnActualVelocity.Click += BtnActualVelocity_Click;
+            btnPositionDependent.Click += BtnPositionDependent_Click;
+            btnSpotDistance.Click += BtnSpotDistance_Click;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            EditorHelper.CreateDevices(out IRtc rtc, out ILaser laser, out IDInput dInExt1, out IDInput dInLaserPort, out IDOutput dOutExt1, out IDOutput dOutExt2, out IDOutput dOutLaserPort, out IPowerMeter powerMeter, out IMarker marker);
+
+            siriusEditorControl1.Scanner = rtc;
+
+            siriusEditorControl1.Laser = laser;
+
+            siriusEditorControl1.DIExt1 = dInExt1;
+            siriusEditorControl1.DOExt1 = dOutExt1;
+            siriusEditorControl1.DOExt2 = dOutExt2;
+            siriusEditorControl1.DILaserPort = dInLaserPort;
+            siriusEditorControl1.DOLaserPort = dOutLaserPort;
+
+            siriusEditorControl1.PowerMeter = powerMeter;
+
+            siriusEditorControl1.Marker = marker;
+
+            marker.Ready(siriusEditorControl1.Document, siriusEditorControl1.View, rtc, laser, powerMeter);
+        }
+
+        private void CreateEntity()
+        {
+            var document = siriusEditorControl1.Document;
+            var text = EntityFactory.CreateText("Arial",
+              FontStyle.Regular,
+              $"AaBbCcDdEeFfGg{Environment.NewLine}0123456789{Environment.NewLine}!@#$%^&*()-+<>",
+              10);
+            text.FontHorizontalAlignment = StringAlignment.Center;
+            text.FontVerticalAlignment = StringAlignment.Center;
+
+            document.ActivePage.ActiveLayer.AddChild(text);
+        }
+        private void BtnSetVelocity_Click(object sender, EventArgs e)
+        {
+            var document = siriusEditorControl1.Document;
+            document.ActNew();
+
+            // create measurement begin
+            var begin = EntityFactory.CreateMeasurementBegin(
+                10 * 1000,
+                new MeasurementChannels[] {
+                    MeasurementChannels.SampleX,
+                    MeasurementChannels.SampleY,
+                    MeasurementChannels.LaserOn,
+                    MeasurementChannels.ExtAO1,
+                },
+                "Set velocity + analog1"
+                );
+            document.ActivePage.ActiveLayer.AddChild(begin);
+
+            CreateEntity();
+
+            var end = EntityFactory.CreateMeasurementEnd();
+            document.ActivePage.ActiveLayer.AddChild(end);
+
+            Debug.Assert(document.ActivePage.ActiveLayer.PenColor == Color.White);
+
+            // Find layer pen for 'White'
+            document.FindByLayerPenColor(System.Drawing.Color.White, out var layerPenWhite);
+
+            //Set veloticy + analog output
+            layerPenWhite.IsALC = true;
+            layerPenWhite.AlcSignal = AutoLaserControlSignals.Analog1;
+            layerPenWhite.AlcMode = AutoLaserControlModes.SetVelocity;
+            layerPenWhite.AlcPercentage100 = 5; //5V
+            layerPenWhite.AlcMinValue = 4; // 4V
+            layerPenWhite.AlcMaxValue = 6; //6V
+            layerPenWhite.AlcByPositionTable = null;
+        }
+
+        private void BtnActualVelocity_Click(object sender, EventArgs e)
+        {
+            var document = siriusEditorControl1.Document;
+            document.ActNew();
+
+            // create measurement begin
+            var begin = EntityFactory.CreateMeasurementBegin(
+                10 * 1000,
+                new MeasurementChannels[] {
+                    MeasurementChannels.SampleX,
+                    MeasurementChannels.SampleY,
+                    MeasurementChannels.LaserOn,
+                    MeasurementChannels.OutputPeriod,
+                },
+                "Actual velocity + frequency"
+                );
+            document.ActivePage.ActiveLayer.AddChild(begin);
+
+            CreateEntity();
+
+            var end = EntityFactory.CreateMeasurementEnd();
+            document.ActivePage.ActiveLayer.AddChild(end);
+
+            Debug.Assert(document.ActivePage.ActiveLayer.PenColor == Color.White);
+
+            // Find layer pen for 'White'
+            document.FindByLayerPenColor(System.Drawing.Color.White, out var layerPenWhite);
+
+            // Actual velocity + frequency
+            layerPenWhite.IsALC = true;
+            layerPenWhite.AlcSignal = AutoLaserControlSignals.Frequency;
+            layerPenWhite.AlcMode = AutoLaserControlModes.ActualVelocity; // Only for iDRIVE scanner products 
+            layerPenWhite.AlcPercentage100 = 50 * 1000; //50KHz
+            layerPenWhite.AlcMinValue = 40 * 1000; //Lower cut off frequency : 40KHz
+            layerPenWhite.AlcMaxValue = 60 * 1000; //Upper cut off frequency : 60KHz
+            layerPenWhite.AlcByPositionTable = null;
+        }
+
+        private void BtnPositionDependent_Click(object sender, EventArgs e)
+        {
+            var document = siriusEditorControl1.Document;
+            document.ActNew();
+
+            // create measurement begin
+            var begin = EntityFactory.CreateMeasurementBegin(
+                10 * 1000,
+                new MeasurementChannels[] {
+                    MeasurementChannels.SampleX,
+                    MeasurementChannels.SampleY,
+                    MeasurementChannels.LaserOn,
+                    MeasurementChannels.ExtAO1,
+                },
+                "Set velocity + analog + position dependent"
+                );
+            document.ActivePage.ActiveLayer.AddChild(begin);
+
+            CreateEntity();
+
+            var end = EntityFactory.CreateMeasurementEnd();
+            document.ActivePage.ActiveLayer.AddChild(end);
+
+            Debug.Assert(document.ActivePage.ActiveLayer.PenColor == Color.White);
+
+            // Find layer pen for 'White'
+            document.FindByLayerPenColor(System.Drawing.Color.White, out var layerPenWhite);
+
+            // Set veloticy + analog output 
+            layerPenWhite.IsALC = true;
+            layerPenWhite.AlcSignal = AutoLaserControlSignals.Analog1;
+            layerPenWhite.AlcMode = AutoLaserControlModes.SetVelocity;
+            layerPenWhite.AlcPercentage100 = 5; //5V
+            layerPenWhite.AlcMinValue = 4; // 4V
+            layerPenWhite.AlcMaxValue = 6; //6V
+            // Distance(or radius) (mm), scale (0~4)
+            var kvList = new List<KeyValuePair<double, double>>();
+            kvList.Add(new KeyValuePair<double, double>(5, 0.9));
+            kvList.Add(new KeyValuePair<double, double>(10, 1));
+            kvList.Add(new KeyValuePair<double, double>(15, 1.1));
+            kvList.Add(new KeyValuePair<double, double>(20, 1.2));
+            kvList.Add(new KeyValuePair<double, double>(25, 1.3));
+            kvList.Add(new KeyValuePair<double, double>(30, 1.4));
+            kvList.Add(new KeyValuePair<double, double>(35, 1.5));
+            kvList.Add(new KeyValuePair<double, double>(40, 1.6));
+            kvList.Add(new KeyValuePair<double, double>(50, 2.0));
+            // + Position dependent
+            layerPenWhite.AlcByPositionTable = kvList.ToArray();
+        }
+
+        private void BtnSpotDistance_Click(object sender, EventArgs e)
+        {
+            var document = siriusEditorControl1.Document;
+            document.ActNew();
+
+            // create measurement begin
+            var begin = EntityFactory.CreateMeasurementBegin(
+                10 * 1000,
+                new MeasurementChannels[] {
+                    MeasurementChannels.SampleX,
+                    MeasurementChannels.SampleY,
+                    MeasurementChannels.LaserOn,
+                    //MeasurementChannels.SpotDistance,
+                },
+                "Spot distance"
+                );
+            document.ActivePage.ActiveLayer.AddChild(begin);
+
+            CreateEntity();
+
+            var end = EntityFactory.CreateMeasurementEnd();
+            document.ActivePage.ActiveLayer.AddChild(end);
+
+            Debug.Assert(document.ActivePage.ActiveLayer.PenColor == Color.White);
+
+            // Find layer pen for 'White'
+            document.FindByLayerPenColor(System.Drawing.Color.White, out var layerPenWhite);
+
+            // Actual velocity + spot distance control
+            layerPenWhite.IsALC = true;
+            layerPenWhite.AlcByPositionTable = null;
+            layerPenWhite.AlcSignal = AutoLaserControlSignals.SpotDistance; //RTC6 + SCANAhead
+            layerPenWhite.AlcMode = AutoLaserControlModes.ActualVelocityWithSCANAhead;
+            double spotDistance = 0.1; 
+            layerPenWhite.AlcPercentage100 = spotDistance;
+            //layerPenWhite.AlcMinValue = 0; // not used at SpotDistance
+            //layerPenWhite.AlcMaxValue = 0; // not used at SpotDistance
+            layerPenWhite.AlcByPositionTable = null;
+        }
+
+    }
+}
