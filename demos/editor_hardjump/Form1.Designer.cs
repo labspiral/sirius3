593bda80274158584f4468a53fc4f017cc55f2d9
hcchoi <hcchoi@spirallab.co.kr>
2025-11-10 15:26:34 +0900

    added) hard jump demo

---
+++ demos/editor_hardjump/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Demos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnNormalJump = new System.Windows.Forms.Button();
+            this.btnHardJump = new System.Windows.Forms.Button();
+            this.siriusEditorControl1 = new SpiralLab.Sirius3.UI.WinForms.SiriusEditorControl();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btnNormalJump);
+            this.flowLayoutPanel1.Controls.Add(this.btnHardJump);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(2254, 55);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // btnNormalJump
+            // 
+            this.btnNormalJump.Location = new System.Drawing.Point(3, 3);
+            this.btnNormalJump.Name = "btnNormalJump";
+            this.btnNormalJump.Size = new System.Drawing.Size(140, 49);
+            this.btnNormalJump.TabIndex = 0;
+            this.btnNormalJump.Text = "Normal Jump";
+            this.btnNormalJump.UseVisualStyleBackColor = true;
+            // 
+            // btnHardJump
+            // 
+            this.btnHardJump.Location = new System.Drawing.Point(149, 3);
+            this.btnHardJump.Name = "btnHardJump";
+            this.btnHardJump.Size = new System.Drawing.Size(140, 49);
+            this.btnHardJump.TabIndex = 1;
+            this.btnHardJump.Text = "Hard Jump";
+            this.btnHardJump.UseVisualStyleBackColor = true;
+            // 
+            // siriusEditorControl1
+            // 
+            this.siriusEditorControl1.AliasName = "NoName";
+            this.siriusEditorControl1.BackColor = System.Drawing.SystemColors.Control;
+            this.siriusEditorControl1.DIExt1 = null;
+            this.siriusEditorControl1.DILaserPort = null;
+            this.siriusEditorControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.siriusEditorControl1.DOExt1 = null;
+            this.siriusEditorControl1.DOExt2 = null;
+            this.siriusEditorControl1.DOLaserPort = null;
+            this.siriusEditorControl1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siriusEditorControl1.Laser = null;
+            this.siriusEditorControl1.Location = new System.Drawing.Point(0, 55);
+            this.siriusEditorControl1.Margin = new System.Windows.Forms.Padding(0);
+            this.siriusEditorControl1.Marker = null;
+            this.siriusEditorControl1.Name = "siriusEditorControl1";
+            this.siriusEditorControl1.PowerMeter = null;
+            this.siriusEditorControl1.Scanner = null;
+            this.siriusEditorControl1.Size = new System.Drawing.Size(2254, 1583);
+            this.siriusEditorControl1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2254, 1638);
+            this.Controls.Add(this.siriusEditorControl1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Sirius3 Demo - (c)SpiralLAB";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private SpiralLab.Sirius3.UI.WinForms.SiriusEditorControl siriusEditorControl1;
+        private Button btnNormalJump;
+        private Button btnHardJump;
+    }
+}
---
+++ demos/editor_hardjump/Form1.cs
@@ -0,0 +1,149 @@
+using System;
+
+using SpiralLab.Sirius3.Document;
+using SpiralLab.Sirius3.Scanner;
+using SpiralLab.Sirius3.IO;
+using SpiralLab.Sirius3.Scanner.Rtc;
+using SpiralLab.Sirius3.PowerMeter;
+using SpiralLab.Sirius3.Laser;
+using SpiralLab.Sirius3.Marker;
+using SpiralLab.Sirius3.Entity;
+using System.Text;
+using SpiralLab.Sirius3.Entity.Hatch;
+using Microsoft.Extensions.Logging;
+using SpiralLab.Sirius3;
+using SpiralLab.Sirius3.Mathematics;
+using SpiralLab.Sirius3.Scanner.Rtc.SyncAxis;
+using System.Diagnostics;
+
+#if OPENTK3
+using OpenTK;
+using DVec2 = OpenTK.Vector2d;
+using DVec3 = OpenTK.Vector3d;
+using DVec4 = OpenTK.Vector4d;
+using DMat3 = OpenTK.Matrix3d;
+using DMat4 = OpenTK.Matrix4d;
+#elif OPENTK4
+using OpenTK.Mathematics;
+using DVec2 = OpenTK.Mathematics.Vector2d;
+using DVec3 = OpenTK.Mathematics.Vector3d;
+using DVec4 = OpenTK.Mathematics.Vector4d;
+using DMat3 = OpenTK.Mathematics.Matrix3d;
+using DMat4 = OpenTK.Mathematics.Matrix4d;
+#endif
+
+namespace Demos
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+
+            btnNormalJump.Click += BtnNormalJump_Click;
+            btnHardJump.Click += BtnHardJump_Click;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            EditorHelper.CreateDevices(out IRtc rtc, out ILaser laser, out IDInput dInExt1, out IDInput dInLaserPort, out IDOutput dOutExt1, out IDOutput dOutExt2, out IDOutput dOutLaserPort, out IPowerMeter powerMeter, out IMarker marker);
+
+            siriusEditorControl1.Scanner = rtc;
+
+            siriusEditorControl1.Laser = laser;
+
+            siriusEditorControl1.DIExt1 = dInExt1;
+            siriusEditorControl1.DOExt1 = dOutExt1;
+            siriusEditorControl1.DOExt2 = dOutExt2;
+            siriusEditorControl1.DILaserPort = dInLaserPort;
+            siriusEditorControl1.DOLaserPort = dOutLaserPort;
+
+            siriusEditorControl1.PowerMeter = powerMeter;
+
+            siriusEditorControl1.Marker = marker;
+
+            marker.Ready(siriusEditorControl1.Document, siriusEditorControl1.View, rtc, laser, powerMeter);
+        }
+
+        private void CreateEntity()
+        {
+            var document = siriusEditorControl1.Document;
+            var text = EntityFactory.CreateText("Arial",
+              FontStyle.Regular,
+              $"Aa{Environment.NewLine}01{Environment.NewLine}!@",
+              10);
+            text.FontHorizontalAlignment = StringAlignment.Center;
+            text.FontVerticalAlignment = StringAlignment.Center;
+            text.PenColor = Color.White;
+
+            document.ActivePage.ActiveLayer.AddChild(text);
+            siriusEditorControl1.View?.DoRender();
+        }
+        private void BtnNormalJump_Click(object sender, EventArgs e)
+        {
+            var document = siriusEditorControl1.Document;
+            document.ActNew();
+
+            // create measurement begin
+            var begin = EntityFactory.CreateMeasurementBegin(
+                10 * 1000,
+                new MeasurementChannels[] {
+                    MeasurementChannels.SampleX,
+                    MeasurementChannels.SampleY,
+                    MeasurementChannels.SampleZ,
+                    MeasurementChannels.LaserOn,
+                },
+                "Normal Jump"
+                );
+            document.ActivePage.ActiveLayer.AddChild(begin);
+
+            CreateEntity();
+
+            var end = EntityFactory.CreateMeasurementEnd();
+            document.ActivePage.ActiveLayer.AddChild(end);
+
+            // Find scanner pen for 'White'
+            document.FindByScannerPenColor(System.Drawing.Color.White, out var scannerPenWhite);
+
+            // Disable Hard Jump 
+            scannerPenWhite.IsHardJump = false;
+            
+            siriusEditorControl1.PropertyGridCtrl.Refresh();
+        }
+
+        private void BtnHardJump_Click(object sender, EventArgs e)
+        {
+            var document = siriusEditorControl1.Document;
+            document.ActNew();
+
+            // create measurement begin
+            var begin = EntityFactory.CreateMeasurementBegin(
+                10 * 1000,
+                new MeasurementChannels[] {
+                    MeasurementChannels.SampleX,
+                    MeasurementChannels.SampleY,
+                    MeasurementChannels.SampleZ,
+                    MeasurementChannels.LaserOn,
+                },
+                "Hard Jump"
+                );
+            document.ActivePage.ActiveLayer.AddChild(begin);
+
+            CreateEntity();
+
+            var end = EntityFactory.CreateMeasurementEnd();
+            document.ActivePage.ActiveLayer.AddChild(end);
+
+            // Find scanner pen for 'White'
+            document.FindByScannerPenColor(System.Drawing.Color.White, out var scannerPenWhite);
+
+            // Disable Hard Jump 
+            scannerPenWhite.IsHardJump = true;
+
+            siriusEditorControl1.PropertyGridCtrl.Refresh();
+        }
+
+    }
+}
