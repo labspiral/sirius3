fae407756b16c24a10a09df1ad75d4ec894e40bd
hcchoi <hcchoi@spirallab.co.kr>
2025-11-20 11:24:01 +0900

    fixed) PerMonitorV2 setting for HiDpi

--- demos/editor_slicer/Program.cs
+++ demos/editor_slicer/Program.cs
@@ -7,6 +7,9 @@ namespace Demos
         [STAThread]
         static void Main()
         {
+#if NET8_0_OR_GREATER
+            Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
+#endif				
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
