1fc2eab64d4422ed58f13cedb2dbf4fa912fba32
hcchoi <hcchoi@spirallab.co.kr>
2026-03-26 14:01:31 +0900

    added) editor_steppermotor demo

---
+++ demos/editor_steppermotor/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Demos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnMove = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.siriusEditorControl1 = new SpiralLab.Sirius3.UI.WinForms.SiriusEditorControl();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnPrepare = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Controls.Add(this.btnHome);
+            this.flowLayoutPanel1.Controls.Add(this.btnMove);
+            this.flowLayoutPanel1.Controls.Add(this.btnStop);
+            this.flowLayoutPanel1.Controls.Add(this.btnPrepare);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1264, 38);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // btnMove
+            // 
+            this.btnMove.AutoSize = true;
+            this.btnMove.Location = new System.Drawing.Point(129, 3);
+            this.btnMove.Name = "btnMove";
+            this.btnMove.Size = new System.Drawing.Size(120, 32);
+            this.btnMove.TabIndex = 0;
+            this.btnMove.Text = "Move";
+            this.btnMove.UseVisualStyleBackColor = true;
+            // 
+            // btnHome
+            // 
+            this.btnHome.AutoSize = true;
+            this.btnHome.Location = new System.Drawing.Point(3, 3);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(120, 32);
+            this.btnHome.TabIndex = 6;
+            this.btnHome.Text = "Home";
+            this.btnHome.UseVisualStyleBackColor = true;
+            // 
+            // siriusEditorControl1
+            // 
+            this.siriusEditorControl1.AliasName = "NoName";
+            this.siriusEditorControl1.AutoSize = true;
+            this.siriusEditorControl1.BackColor = System.Drawing.SystemColors.Control;
+            this.siriusEditorControl1.DIExt1 = null;
+            this.siriusEditorControl1.DILaserPort = null;
+            this.siriusEditorControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.siriusEditorControl1.DOExt1 = null;
+            this.siriusEditorControl1.DOExt2 = null;
+            this.siriusEditorControl1.DOLaserPort = null;
+            this.siriusEditorControl1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siriusEditorControl1.Laser = null;
+            this.siriusEditorControl1.Location = new System.Drawing.Point(0, 38);
+            this.siriusEditorControl1.Margin = new System.Windows.Forms.Padding(0);
+            this.siriusEditorControl1.Marker = null;
+            this.siriusEditorControl1.Name = "siriusEditorControl1";
+            this.siriusEditorControl1.PowerMeter = null;
+            this.siriusEditorControl1.Scanner = null;
+            this.siriusEditorControl1.Size = new System.Drawing.Size(1264, 823);
+            this.siriusEditorControl1.TabIndex = 2;
+            // 
+            // btnStop
+            // 
+            this.btnStop.AutoSize = true;
+            this.btnStop.Location = new System.Drawing.Point(255, 3);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(120, 32);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // btnPrepare
+            // 
+            this.btnPrepare.AutoSize = true;
+            this.btnPrepare.Location = new System.Drawing.Point(381, 3);
+            this.btnPrepare.Name = "btnPrepare";
+            this.btnPrepare.Size = new System.Drawing.Size(120, 32);
+            this.btnPrepare.TabIndex = 8;
+            this.btnPrepare.Text = "Prepare";
+            this.btnPrepare.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 861);
+            this.Controls.Add(this.siriusEditorControl1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Sirius3 Demo - (c)SpiralLAB";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private SpiralLab.Sirius3.UI.WinForms.SiriusEditorControl siriusEditorControl1;
+        private Button btnMove;
+        private Button btnHome;
+        private Button btnStop;
+        private Button btnPrepare;
+    }
+}
---
+++ demos/editor_steppermotor/Form1.cs
@@ -0,0 +1,225 @@
+using System;
+
+using SpiralLab.Sirius3.Document;
+using SpiralLab.Sirius3.Scanner;
+using SpiralLab.Sirius3.IO;
+using SpiralLab.Sirius3.Scanner.Rtc;
+using SpiralLab.Sirius3.PowerMeter;
+using SpiralLab.Sirius3.Laser;
+using SpiralLab.Sirius3.Marker;
+using SpiralLab.Sirius3.Entity;
+using System.Text;
+using SpiralLab.Sirius3.Entity.Hatch;
+using Microsoft.Extensions.Logging;
+using SpiralLab.Sirius3;
+using SpiralLab.Sirius3.Mathematics;
+using SpiralLab.Sirius3.Scanner.Rtc.SyncAxis;
+using System.Diagnostics;
+
+#if OPENTK3
+using OpenTK;
+using DVec2 = OpenTK.Vector2d;
+using DVec3 = OpenTK.Vector3d;
+using DVec4 = OpenTK.Vector4d;
+using DMat3 = OpenTK.Matrix3d;
+using DMat4 = OpenTK.Matrix4d;
+#elif OPENTK4
+using OpenTK.Mathematics;
+using DVec2 = OpenTK.Mathematics.Vector2d;
+using DVec3 = OpenTK.Mathematics.Vector3d;
+using DVec4 = OpenTK.Mathematics.Vector4d;
+using DMat3 = OpenTK.Mathematics.Matrix3d;
+using DMat4 = OpenTK.Mathematics.Matrix4d;
+#endif
+
+namespace Demos
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+            this.FormClosing += (s, e) =>
+            {
+                var dlgResult = MessageBox.Show(this, $"Do you really want to terminate program ?", "WARNING", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dlgResult != DialogResult.Yes)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+
+                // Dispose instances 
+                siriusEditorControl1.DisposeDevices();
+
+                // Clean up SIRIUS3 library
+                SpiralLab.Sirius3.Core.Cleanup();
+            };
+
+            btnHome.Click += BtnHome_Click;
+            btnMove.Click += BtnMove_Click;
+            btnStop.Click += BtnStop_Click;
+            btnPrepare.Click += BtnPrepare_Click;
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            EditorHelper.CreateDevices(out IRtc rtc, out ILaser laser, out IDInput dInExt1, out IDInput dInLaserPort, out IDOutput dOutExt1, out IDOutput dOutExt2, out IDOutput dOutLaserPort, out IPowerMeter powerMeter, out IMarker marker);
+
+            ConfigStepperMotor(rtc);
+
+            siriusEditorControl1.Scanner = rtc;
+            siriusEditorControl1.Laser = laser;
+            siriusEditorControl1.DIExt1 = dInExt1;
+            siriusEditorControl1.DOExt1 = dOutExt1;
+            siriusEditorControl1.DOExt2 = dOutExt2;
+            siriusEditorControl1.DILaserPort = dInLaserPort;
+            siriusEditorControl1.DOLaserPort = dOutLaserPort;
+            siriusEditorControl1.PowerMeter = powerMeter;
+            siriusEditorControl1.Marker = marker;
+
+            marker.Ready(siriusEditorControl1.Document, siriusEditorControl1.View, rtc, laser, powerMeter);
+        }
+
+        private void ConfigStepperMotor(IRtc rtc)
+        {
+            // Must be support STEPPER PORT at RTC card
+            // RTC5, RTC6 only
+            var rtcStepper = rtc as IRtcStepper;
+            Debug.Assert(rtcStepper != null);
+
+            rtcStepper.StepperScaleUnits = new StepperUnits[2]
+            {
+                StepperUnits.MilliMeter,
+                StepperUnits.MilliMeter,
+            };
+
+            //// for Rotary motor
+            //rtcStepper.StepperScaleUnits = new StepperUnits[2]
+            //{
+            //    StepperUnits.Degree,
+            //    StepperUnits.Degree,
+            //};
+
+            rtcStepper.StepperScaleFactors = new double[2]
+            {
+                4000, // 4000 steps/mm
+                4000, // 4000 steps/mm
+            };
+
+            //// for Rotary motor
+            //rtcStepper.StepperScaleFactors = new double[2]
+            //{
+            //    3600, // 3600 steps/rev
+            //    3600, // 3600 steps/rev
+            //};
+
+            rtcStepper.OnStepperInitialized -= RtcStepper_OnStepperInitialized;
+            rtcStepper.OnStepperInitialized += RtcStepper_OnStepperInitialized;
+        }
+
+        private void RtcStepper_OnStepperInitialized(IRtcStepper rtcStepper, uint motorNo, bool initialized)
+        {
+            if (initialized) 
+                MessageBox.Show(this, "Complete to Search Home", "Stepper", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show(this, "Fail to Search Home", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void BtnHome_Click(object sender, EventArgs e)
+        {
+            var rtcStepper = siriusEditorControl1.Scanner as IRtcStepper;
+            btnHome.Enabled = btnMove.Enabled  = false;
+
+            Task.Run(() =>
+            {
+                double vel1 = 10;
+                double vel2 = 1;
+                double tol1 = 10;
+                double tol2 = 1;
+                double pos1 = 0; // mm or deg
+
+                var timeOutSec = SpiralLab.Sirius3.UI.Config.StepperReferenceRunTimeOut; // default: 30s
+
+                try
+                {
+                    rtcStepper.CtlStepperReferenceRun(StepperAxes.Axis1, StepperDirections.Negativity, vel1, tol1, vel2, tol2, pos1, timeOutSec);
+                    
+                }
+                finally
+                {
+                    if (!this.IsDisposed && this.IsHandleCreated)
+                    {
+                        this.BeginInvoke(new MethodInvoker(delegate ()
+                        {
+                            btnHome.Enabled = btnMove.Enabled = true;
+                        }));
+                    }
+                }
+            });
+        }
+
+        private void BtnMove_Click(object sender, EventArgs e)
+        {
+            var rtcStepper = siriusEditorControl1.Scanner as IRtcStepper;
+
+            double vel = 10;
+            StepperMoveTypes moveType = StepperMoveTypes.Absolute;
+            double pos = 10; //mm or deg
+
+            rtcStepper.CtlStepperVelocity(vel, -1);
+            rtcStepper.CtlStepperMove(StepperAxes.Axis1, moveType, pos, 0);
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            var rtcStepper = siriusEditorControl1.Scanner as IRtcStepper;
+            rtcStepper.CtlStepperMoveStop(StepperCombinedAxes.Axis1);
+        }
+
+
+        private void BtnPrepare_Click(object sender, EventArgs e)
+        {
+            var document = siriusEditorControl1.Document;
+            var marker = siriusEditorControl1.Marker;
+            var rtcStepper = siriusEditorControl1.Scanner as IRtcStepper;
+
+            //siriusEditorControl1.Document.ActNew();
+
+            {
+                double pos = 10; //mm or deg
+                double vel = 5; // mm/s or deg/s
+                var stepperMove = EntityFactory.CreateStepperMove(StepperAxes.Axis1, StepperMoveTypes.Absolute, pos, vel);
+                document.ActivePage?.ActiveLayer?.AddChild(stepperMove);
+
+                var stepperWait = EntityFactory.CreateStepperWait(StepperCombinedAxes.Axis1, 2_000);
+                document.ActivePage?.ActiveLayer?.AddChild(stepperWait);
+
+                var rect = EntityFactory.CreateRectangle(0, 0, 10, 10);
+                document.ActivePage?.ActiveLayer?.AddChild(rect);
+            }
+
+            {
+                double pos = 20; //mm or deg
+                double vel = 5; // mm/s or deg/s
+                var stepperMove = EntityFactory.CreateStepperMove(StepperAxes.Axis1, StepperMoveTypes.Absolute, pos, vel);
+                document.ActivePage?.ActiveLayer?.AddChild(stepperMove);
+
+                var stepperWait = EntityFactory.CreateStepperWait(StepperCombinedAxes.Axis1, 2_000);
+                document.ActivePage?.ActiveLayer?.AddChild(stepperWait);
+
+                var circle = EntityFactory.CreateArc(0, 0, 5, 0, 360);
+                document.ActivePage?.ActiveLayer?.AddChild(circle);
+            }
+
+
+            siriusEditorControl1.View?.DoRender();
+
+            //marker.Ready(siriusEditorControl1.Document);
+            //marker.Start();
+        }
+    }
+}
